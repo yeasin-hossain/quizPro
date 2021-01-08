41ba7bb334d3dec191fd5fcfae9a4025473eb06d
yeasin-hossaom <yeasinhossain475@gmail.com>
2021-01-08 06:22:00 +0600

    bug fixe

--- game.js
+++ game.js
@@ -61,7 +61,7 @@ startGame = () =>{
 getNewQuestion = () =>{
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTION){
         localStorage.setItem('mostRecentScore', score);
-        return window.location.assign('/end.html');
+        return window.location.assign('end.html');
     }
     questionCounter++;
     progressText.innerText  = `Question ${questionCounter}/${MAX_QUESTION}`;
